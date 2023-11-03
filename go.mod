5718caf1f82394c2e21d5a9ad42fb5bd566223f6
palmcivet7 <palmcivet@proton.me>
2023-11-03 15:52:11 +0000

    create wallet

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/palmcivet7/go-blockchain
+
+go 1.21.0
+
+require (
+	github.com/btcsuite/btcutil v1.0.2 // indirect
+	golang.org/x/crypto v0.14.0 // indirect
+)
---
+++ wallet/wallet.go
@@ -0,0 +1,117 @@
+package wallet
+
+import (
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+
+	"github.com/btcsuite/btcutil/base58"
+	"github.com/palmcivet7/go-blockchain/utils"
+)
+
+type Wallet struct {
+	privateKey        *ecdsa.PrivateKey
+	publicKey         *ecdsa.PublicKey
+	blockchainAddress string
+}
+
+func NewWallet() *Wallet {
+	w := new(Wallet)
+	privateKey, _ := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	w.privateKey = privateKey
+	w.publicKey = &w.privateKey.PublicKey
+
+	h2 := sha256.New()
+	h2.Write(w.publicKey.X.Bytes())
+	h2.Write(w.publicKey.Y.Bytes())
+	digest2 := h2.Sum(nil)
+
+	h3 := sha256.New()
+	h3.Write(digest2)
+	digest3 := h3.Sum(nil)
+
+	vd4 := make([]byte, 33) // Adjusted size from 21 to 33
+	vd4[0] = 0x00
+	copy(vd4[1:], digest3[:])
+
+	h5 := sha256.New()
+	h5.Write(vd4)
+	digest5 := h5.Sum(nil)
+
+	h6 := sha256.New()
+	h6.Write(digest5)
+	digest6 := h6.Sum(nil)
+
+	chsum := digest6[:4]
+
+	dc8 := make([]byte, 37) // Adjusted size from 25 to 37
+	copy(dc8[:33], vd4)     // Adjusted size from 21 to 33
+	copy(dc8[33:], chsum)
+
+	address := base58.Encode(dc8)
+	w.blockchainAddress = address
+	return w
+}
+
+func (w *Wallet) PrivateKey() *ecdsa.PrivateKey {
+	return w.privateKey
+}
+
+func (w *Wallet) PrivateKeyStr() string {
+	return fmt.Sprintf("%x", w.privateKey.D.Bytes())
+}
+
+func (w *Wallet) PublicKey() *ecdsa.PublicKey {
+	return w.publicKey
+}
+
+func (w *Wallet) PublicKeyStr() string {
+	return fmt.Sprintf("%x%x", w.publicKey.X.Bytes(), w.publicKey.Y.Bytes())
+}
+
+func (w *Wallet) BlockchainAddress() string {
+	return w.blockchainAddress
+}
+
+type Transaction struct {
+	senderPrivateKey			*ecdsa.PrivateKey
+	senderPublicKey				*ecdsa.PublicKey
+	senderBlockchainAddress		string
+	receiverBlockchainAddress	string
+	value						float64
+}
+
+func NewTransaction(
+	privateKey *ecdsa.PrivateKey,
+	publicKey *ecdsa.PublicKey,
+	sender string,
+	receiver string,
+	value float64,
+) *Transaction {
+	return &Transaction{
+		privateKey, publicKey, sender, receiver, value,
+	}
+} 
+
+func (t *Transaction) GenerateSignature() *utils.Signature {
+	m, _ := json.Marshal(t)
+	h := sha256.Sum256([]byte(m))
+	r, s, _ := ecdsa.Sign(rand.Reader, t.senderPrivateKey, h[:])
+	return &utils.Signature{r, s}
+}
+
+func (t *Transaction) MarshalJSON() ([]byte, error) {
+	return json.Marshal(struct{
+		Sender		string	`json:"sender_address"`
+		Receiver	string	`json:"receiver_address"`
+		Value		float64	`json:"value"`
+	}{
+		Sender: t.senderBlockchainAddress,
+		Receiver: t.receiverBlockchainAddress,
+		Value: t.value,
+	})
+}
+
