4803e0d401f60312b49093086fcfb8370e468076
palmcivet7 <palmcivet@proton.me>
2023-11-04 13:04:42 +0000

    begin blockchain server api

---
+++ wallet_server/wallet_server.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+	"text/template"
+
+	"github.com/palmcivet7/go-blockchain/utils"
+	"github.com/palmcivet7/go-blockchain/wallet"
+)
+
+const tempDir = "templates"
+
+type WalletServer struct {
+	port		uint16
+	gateway		string
+}
+
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port, gateway}
+}
+
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+func (ws *WalletServer) Gateway() string {
+	return ws.gateway
+}
+
+func (ws *WalletServer) Index(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodGet:
+		t, err := template.ParseFiles(path.Join(tempDir, "index.html"))
+		if err != nil {
+			log.Printf("Error parsing template: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+
+		if err := t.Execute(w, ""); err != nil {
+			log.Printf("Error executing template: %v", err)
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	default:
+		log.Printf("ERROR: Invalid HTTP Method")
+	}
+}
+
+func (ws *WalletServer) Wallet(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		w.Header().Add("Content-Type", "application-json")
+		myWallet := wallet.NewWallet()
+		m, _ := myWallet.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid HTTP Method")
+	}
+}
+
+func (ws *WalletServer) CreateTransaction(w http.ResponseWriter, r *http.Request) {
+	switch r.Method{
+	case http.MethodPost:
+		io.WriteString(w, string(utils.JsonStatus("success")))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid HTTP Method")
+	}
+}
+
+
+func (ws *WalletServer) Run() {
+	http.HandleFunc("/", ws.Index)
+	http.HandleFunc("/wallet", ws.Wallet)
+	http.HandleFunc("/transaction", ws.CreateTransaction)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
+}
